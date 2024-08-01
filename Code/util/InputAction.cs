8d4fca7e0f609184fd032c291a4611a15ef6f64b
klandestino7 <klandestino6@hotmail.com>
2024-08-01 17:45:16 -0300

    feat: add input actions

---
+++ Code/util/InputAction.cs
@@ -0,0 +1,16 @@
+
+public static class InputAction
+{
+	public const string LeftClick = "Mouse1";
+	public const string RightClick = "Mouse2";
+	public const string Jump = "Jump";
+	public const string Duck = "Duck";
+	public const string Walk = "Walk";
+    public const string Sprint = "Sprint";
+    public const string Use = "Use";
+
+	public const string Voice = "Voice";
+	public const string Zoom = "Zoom";
+	public const string Ragdoll = "Ragdoll";
+	public const string Tasks = "Tasks";
+}
