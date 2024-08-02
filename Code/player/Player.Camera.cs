de7e94c1f7e569782c2d5c387ee581da7da75baa
klandestino7 <klandestino6@hotmail.com>
2024-08-02 11:50:13 -0300

    tweak: disable old code

--- Code/player/Player.Camera.cs
+++ Code/player/Player.Camera.cs
@@ -1,101 +1,101 @@
-using Sandbox;
-using Sandbox.Citizen;
-using Sandbox.Diagnostics;
-using System;
-using System.Linq;
-using System.Numerics;
+// using Sandbox;
+// using Sandbox.Citizen;
+// using Sandbox.Diagnostics;
+// using System;
+// using System.Linq;
+// using System.Numerics;
 
 
-public partial class Player : Component
-{
-	[Property] public float WheelSpeed => 30f;
-	[Property] public Vector2 CameraDistance => new( 500, 1300 );
-	[Property] public Vector2 PitchClamp => new( 45, 45 );
-	public float Zoom { get; set; } = 30f;
+// public partial class Player : Component
+// {
+// 	[Property] public float WheelSpeed => 30f;
+// 	[Property] public Vector2 CameraDistance => new( 500, 1300 );
+// 	[Property] public Vector2 PitchClamp => new( 45, 45 );
+// 	public float Zoom { get; set; } = 30f;
 
-	[Sync]
-	public Angles EyeAngles { get; set; }
+// 	[Sync]
+// 	public Angles EyeAngles { get; set; }
 
-	public Vector2 CursorScreenPosition { get; set; }
-	float OrbitDistance = 500f;
-	float TargetOrbitDistance = 600f;
-	Angles OrbitAngles = Angles.Zero;
+// 	public Vector2 CursorScreenPosition { get; set; }
+// 	float OrbitDistance = 500f;
+// 	float TargetOrbitDistance = 600f;
+// 	Angles OrbitAngles = Angles.Zero;
 
-	protected void UpdateCamera()
-	{
-		if ( Camera == null )
-			return;
-		var cc = GameObject.Components.Get<CharacterController>();
+// 	protected void UpdateCamera()
+// 	{
+// 		if ( Camera == null )
+// 			return;
+// 		var cc = GameObject.Components.Get<CharacterController>();
 
-		if ( DebugCamera )
-		{
-			DebugCameraPosition += Camera.Transform.Rotation * Input.AnalogMove * Time.Delta * ((Input.Down( InputAction.Sprint ) ? 1000f : (Input.Down( InputAction.Walk ) ? 150f : 400f)));
-			Camera.Transform.Position = Camera.Transform.Position.LerpTo( DebugCameraPosition, Time.Delta * 5f );
-			Camera.Transform.Rotation = Rotation.Lerp( Camera.Transform.Rotation, DebugCameraAngles, Time.Delta * 5f );
-		}
-		else
-		{
-			var eyes = EyePos.Transform;
-			var rot = Transform.Rotation;
-			var oldEyeRot = Camera.Transform.Rotation;
-			var newEyeRot = IsRagdolled ? eyes.Rotation : EyeAngles.ToRotation();
-			var oldEyePos = Camera.Transform.Position;
-			var newEyePos = eyes.Position + (IsRagdolled ? 0f : rot.Forward * 2.4f);
+// 		if ( DebugCamera )
+// 		{
+// 			DebugCameraPosition += Camera.Transform.Rotation * Input.AnalogMove * Time.Delta * ((Input.Down( InputAction.Sprint ) ? 1000f : (Input.Down( InputAction.Walk ) ? 150f : 400f)));
+// 			Camera.Transform.Position = Camera.Transform.Position.LerpTo( DebugCameraPosition, Time.Delta * 5f );
+// 			Camera.Transform.Rotation = Rotation.Lerp( Camera.Transform.Rotation, DebugCameraAngles, Time.Delta * 5f );
+// 		}
+// 		else
+// 		{
+// 			var eyes = EyePos.Transform;
+// 			var rot = Transform.Rotation;
+// 			var oldEyeRot = Camera.Transform.Rotation;
+// 			var newEyeRot = IsRagdolled ? eyes.Rotation : EyeAngles.ToRotation();
+// 			var oldEyePos = Camera.Transform.Position;
+// 			var newEyePos = eyes.Position + (IsRagdolled ? 0f : rot.Forward * 2.4f);
 
-			Camera.Transform.Position = IsRagdolled ? Vector3.Lerp( oldEyePos, newEyePos, Time.Delta * 10f ) : newEyePos;
-			Camera.Transform.Rotation = IsRagdolled ? Rotation.Lerp( oldEyeRot, newEyeRot, Time.Delta * 5f ) : newEyeRot;
-			var newRot = Rotation.FromRoll( Vector3.Dot( Transform.Rotation.Right, cc.Velocity.Normal ) ) * 2f;
-			_lastRot = Rotation.Lerp( _lastRot, newRot, Time.Delta * 5f );
-			Camera.Transform.Rotation *= _lastRot;
-		}
+// 			Camera.Transform.Position = IsRagdolled ? Vector3.Lerp( oldEyePos, newEyePos, Time.Delta * 10f ) : newEyePos;
+// 			Camera.Transform.Rotation = IsRagdolled ? Rotation.Lerp( oldEyeRot, newEyeRot, Time.Delta * 5f ) : newEyeRot;
+// 			var newRot = Rotation.FromRoll( Vector3.Dot( Transform.Rotation.Right, cc.Velocity.Normal ) ) * 2f;
+// 			_lastRot = Rotation.Lerp( _lastRot, newRot, Time.Delta * 5f );
+// 			Camera.Transform.Rotation *= _lastRot;
+// 		}
 
-		Camera.FieldOfView = MathX.LerpTo( Camera.FieldOfView, Input.Down( InputAction.Zoom ) ? Zoom : 90f, 10f * Time.Delta );
-		Camera.ZNear = 2.5f;
+// 		Camera.FieldOfView = MathX.LerpTo( Camera.FieldOfView, Input.Down( InputAction.Zoom ) ? Zoom : 90f, 10f * Time.Delta );
+// 		Camera.ZNear = 2.5f;
 
-		UpdateHeadVisibility();
-	}
+// 		UpdateHeadVisibility();
+// 	}
 
-	public void UpdateHeadVisibility()
-	{
-		var eyes = PlayerBody.GetAttachment( "eyes" ) ?? Transform.World;
-		var rot = Transform.Rotation;
+// 	public void UpdateHeadVisibility()
+// 	{
+// 		var eyes = PlayerBody.GetAttachment( "eyes" ) ?? Transform.World;
+// 		var rot = Transform.Rotation;
 
-		if ( HideHead )
-			PlayerBody?.SceneModel?.SetBoneWorldTransform( 7,
-				new Transform( eyes.Position + rot.Backward * 10, Rotation.Identity, 0 ) );
+// 		if ( HideHead )
+// 			PlayerBody?.SceneModel?.SetBoneWorldTransform( 7,
+// 				new Transform( eyes.Position + rot.Backward * 10, Rotation.Identity, 0 ) );
 
-		// Hide face and head clothing.
-		// var face = (Inventory.EquippedItems?.ElementAtOrDefault( (int)EquipSlot.Face ) as ItemEquipment)?.Renderer;
-		// if ( face != null )
-		// 	face.RenderType = HideHead ? ModelPlayerBody.ShadowRenderType.ShadowsOnly : ModelPlayerBody.ShadowRenderType.On;
+// 		// Hide face and head clothing.
+// 		// var face = (Inventory.EquippedItems?.ElementAtOrDefault( (int)EquipSlot.Face ) as ItemEquipment)?.Renderer;
+// 		// if ( face != null )
+// 		// 	face.RenderType = HideHead ? ModelPlayerBody.ShadowRenderType.ShadowsOnly : ModelPlayerBody.ShadowRenderType.On;
 
-		// var head = (Inventory.EquippedItems?.ElementAtOrDefault( (int)EquipSlot.Head ) as ItemEquipment)?.Renderer;
-		// if ( head != null )
-		// 	head.RenderType = HideHead ? ModelPlayerBody.ShadowRenderType.ShadowsOnly : ModelPlayerBody.ShadowRenderType.On;
-	}
+// 		// var head = (Inventory.EquippedItems?.ElementAtOrDefault( (int)EquipSlot.Head ) as ItemEquipment)?.Renderer;
+// 		// if ( head != null )
+// 		// 	head.RenderType = HideHead ? ModelPlayerBody.ShadowRenderType.ShadowsOnly : ModelPlayerBody.ShadowRenderType.On;
+// 	}
 
 
-	// protected override void OnFixedUpdate()
-	// {
-	// 	var wheel = Input.MouseWheel;
+// 	// protected override void OnFixedUpdate()
+// 	// {
+// 	// 	var wheel = Input.MouseWheel;
 
-	// 	if ( wheel.y != 0 )
-	// 	{
-	// 		TargetOrbitDistance -= wheel.y * WheelSpeed;
-	// 		TargetOrbitDistance = TargetOrbitDistance.Clamp( CameraDistance.x, CameraDistance.y );
-	// 	}
+// 	// 	if ( wheel.y != 0 )
+// 	// 	{
+// 	// 		TargetOrbitDistance -= wheel.y * WheelSpeed;
+// 	// 		TargetOrbitDistance = TargetOrbitDistance.Clamp( CameraDistance.x, CameraDistance.y );
+// 	// 	}
 
-	// 	OrbitDistance = OrbitDistance.LerpTo( TargetOrbitDistance, Time.Delta * 10f );
+// 	// 	OrbitDistance = OrbitDistance.LerpTo( TargetOrbitDistance, Time.Delta * 10f );
 
-	// 	if ( Input.UsingController || Input.Down( "MoveCamera" ) )
-	// 	{
-	// 		OrbitAngles.yaw += Input.AnalogLook.yaw * 5f;
-	// 		OrbitAngles.pitch += Input.AnalogLook.pitch * 5f;
-	// 		OrbitAngles = OrbitAngles.Normal;
-	// 	}
+// 	// 	if ( Input.UsingController || Input.Down( "MoveCamera" ) )
+// 	// 	{
+// 	// 		OrbitAngles.yaw += Input.AnalogLook.yaw * 5f;
+// 	// 		OrbitAngles.pitch += Input.AnalogLook.pitch * 5f;
+// 	// 		OrbitAngles = OrbitAngles.Normal;
+// 	// 	}
 
-	// 	EyeAngles = OrbitAngles.WithPitch( 0f );
+// 	// 	EyeAngles = OrbitAngles.WithPitch( 0f );
 
-	// 	OrbitAngles.pitch = OrbitAngles.pitch.Clamp( PitchClamp.x, PitchClamp.y );
-	// }
-}
+// 	// 	OrbitAngles.pitch = OrbitAngles.pitch.Clamp( PitchClamp.x, PitchClamp.y );
+// 	// }
+// }
--- Code/player/Player.Controller.cs
+++ Code/player/Player.Controller.cs
@@ -1,262 +1,262 @@
-using Sandbox;
-using Sandbox.Citizen;
-using Sandbox.Diagnostics;
-using System;
-using System.Linq;
-using System.Numerics;
+// using Sandbox;
+// using Sandbox.Citizen;
+// using Sandbox.Diagnostics;
+// using System;
+// using System.Linq;
+// using System.Numerics;
 
-public partial class Player : Component
-{
-	[Property] public Vector3 Gravity { get; set; } = new Vector3( 0, 0, 800 );
-	[Property] public bool FirstPerson { get; set; }
+// public partial class Player : Component
+// {
+// 	[Property] public Vector3 Gravity { get; set; } = new Vector3( 0, 0, 800 );
+// 	[Property] public bool FirstPerson { get; set; }
 
-		/// <summary>
-	/// How fast you move when holding the sprint button
-	/// </summary>
-	[Property, Sync]
-	[Category( "Movement" )]
-	[Range( 0f, 800f, 1f )]
-	public float SprintSpeed { get; set; } = 280f;
+// 		/// <summary>
+// 	/// How fast you move when holding the sprint button
+// 	/// </summary>
+// 	[Property, Sync]
+// 	[Category( "Movement" )]
+// 	[Range( 0f, 800f, 1f )]
+// 	public float SprintSpeed { get; set; } = 280f;
 
-	/// <summary>
-	/// How fast you move when holding the walk button
-	/// </summary>
-	[Property, Sync]
-	[Category( "Movement" )]
-	[Range( 0f, 200f, 1f )]
-	public float WalkSpeed { get; set; } = 60f;
+// 	/// <summary>
+// 	/// How fast you move when holding the walk button
+// 	/// </summary>
+// 	[Property, Sync]
+// 	[Category( "Movement" )]
+// 	[Range( 0f, 200f, 1f )]
+// 	public float WalkSpeed { get; set; } = 60f;
 
-	/// <summary>
-	/// How fast you move when holding the duck button
-	/// </summary>
-	[Property, Sync]
-	[Category( "Movement" )]
-	[Range( 0f, 200f, 1f )]
-	public float DuckSpeed { get; set; } = 60f;
+// 	/// <summary>
+// 	/// How fast you move when holding the duck button
+// 	/// </summary>
+// 	[Property, Sync]
+// 	[Category( "Movement" )]
+// 	[Range( 0f, 200f, 1f )]
+// 	public float DuckSpeed { get; set; } = 60f;
 
-	/// <summary>
-	/// How high you can jump
-	/// </summary>
-	[Property, Sync]
-	[Category( "Movement" )]
-	[Range( 0f, 800f, 1f )]
-	public float JumpStrength { get; set; } = 200f;
+// 	/// <summary>
+// 	/// How high you can jump
+// 	/// </summary>
+// 	[Property, Sync]
+// 	[Category( "Movement" )]
+// 	[Range( 0f, 800f, 1f )]
+// 	public float JumpStrength { get; set; } = 200f;
 
-	public Vector3 MoveDirection { get;  set; }
-	public const float DUCK_HEIGHT = 58f;
-	public const float HEIGHT = 72f;
-	[Sync] public bool Ducking { get; set; }
-	Rotation _lastRot;
+// 	public Vector3 MoveDirection { get;  set; }
+// 	public const float DUCK_HEIGHT = 58f;
+// 	public const float HEIGHT = 72f;
+// 	[Sync] public bool Ducking { get; set; }
+// 	Rotation _lastRot;
 
-	bool _blockMovements = false;
-	/// <summary>
-	/// Block both inputs and mouse aiming
-	/// </summary>
-	[Sync]
-	public bool BlockMovements
-	{
-		get => DebugCamera ? true : _blockMovements;
-		set => _blockMovements = value;
-	}
-
-	
-	bool _blockMouseAim = false;
-
-	/// <summary>
-	/// Block mouse aiming
-	/// </summary>
-	[Sync]
-	public bool BlockMouseAim
-	{
-		get => BlockMovements || _blockMouseAim;
-		set => _blockMouseAim = value;
-	}
-
-	// public bool IsRunning { get; set; }
-	// protected override void OnEnabled()
-	// {
-	// 	base.OnEnabled();
-
-	// 	if ( IsProxy )
-	// 		return;
-
-	// 	var cam = Scene.GetAllComponents<CameraComponent>().FirstOrDefault();
-	// 	if ( cam is not null )
-	// 	{
-	// 		var ee = cam.Transform.Rotation.Angles();
-	// 		ee.roll = 0;
-	// 		EyeAngles = ee;
-	// 	}
-	// }
-
-	protected void ControllerOnUpdate()
-	{
-		var movement = Input.AnalogMove.Normal;
-		var angles = EyeAngles;
-		var newRotationAngles = new Angles( 0, EyeAngles.yaw, 0 );
-
-		if ( Input.Down( "run" ) )
-			Body.Transform.Rotation = newRotationAngles;
-		else
-			Body.Transform.Rotation = Rotation.Lerp(  newRotationAngles , angles.ToRotation(), Time.Delta * 2f);
-
-		var isWalking = Input.Down( InputAction.Walk );
-
-		var wishSpeed = Ducking ? DuckSpeed : isWalking ? WalkSpeed : SprintSpeed;
-
-		var wishVelocity = Input.AnalogMove.Normal * wishSpeed * EyeAngles.WithPitch( 0f );
-		MoveDirection = wishVelocity;
-	}
-
-	[Broadcast]
-	public void OnJump( float floatValue, string dataString, object[] objects, Vector3 position )
-	{
-		AnimationHelper?.TriggerJump();
-	}
-
-	float fJumps;
-	protected void UpdateController()
-	{
-		if ( IsProxy )
-			return;
-
-		BuildWishVelocity();
-
-		var cc = GameObject.Components.Get<CharacterController>();
-
-		if ( cc.IsOnGround && Input.Down( "Jump" ) )
-		{
-			float flGroundFactor = 1.0f;
-			float flMul = 268.3281572999747f * 1.2f;
-
-			cc.Punch( Vector3.Up * flMul * flGroundFactor );
-
-			OnJump( fJumps, "Hello", new object[] { Time.Now.ToString(), 43.0f }, Vector3.Random );
-
-			fJumps += 1.0f;
-		}
-
-		if ( cc.IsOnGround )
-		{
-			cc.Velocity = cc.Velocity.WithZ( 0 );
-			cc.Accelerate( WishVelocity );
-			cc.ApplyFriction( 4.0f );
-		}
-		else
-		{
-			cc.Velocity -= Gravity * Time.Delta * 0.5f;
-			cc.Accelerate( WishVelocity.ClampLength( 50 ) );
-			cc.ApplyFriction( 0.1f );
-		}
-
-		cc.Move();
-		// MoveHelper.Move();
-
-		if ( !cc.IsOnGround )
-		{
-			cc.Velocity -= Gravity * Time.Delta * 0.5f;
-		}
-		else
-		{
-			cc.Velocity = cc.Velocity.WithZ( 0 );
-		}
-	}
-
-	private Angles _currentRecoil;
-	private Angles _previousRecoil;
-	protected void UpdateAngles()
-	{
-		if ( DebugCamera )
-		{
-			DebugCameraAngles += Input.AnalogLook;
-		}
-		if ( BlockMouseAim ) return;
-
-		var before = EyeAngles;
-		var ang = EyeAngles;
-		ang += Input.AnalogLook;
-		ang += _currentRecoil - _previousRecoil; // Apply recoil to eye angles.
-		ang.pitch = ang.pitch.Clamp( -89, 89 );
-
-		EyeAngles = ang;
-
-		// Calculate recoil.
-		var diff = (before - ang).AsVector3().Abs().Length.Clamp( 1, 15 );
-		_previousRecoil = _currentRecoil;
-		_currentRecoil = _currentRecoil.LerpTo( Angles.Zero, diff * Time.Delta );
-	}
+// 	bool _blockMovements = false;
+// 	/// <summary>
+// 	/// Block both inputs and mouse aiming
+// 	/// </summary>
+// 	[Sync]
+// 	public bool BlockMovements
+// 	{
+// 		get => DebugCamera ? true : _blockMovements;
+// 		set => _blockMovements = value;
+// 	}
 
 	
-	// protected void UpdateMovement()
-	// {
-	// 	if ( MoveHelper == null ) return;
+// 	bool _blockMouseAim = false;
 
-	// 	var previousFallSpeed = Velocity.z;
-	// 	var isWalking = Input.Down( InputAction.Walk );
+// 	/// <summary>
+// 	/// Block mouse aiming
+// 	/// </summary>
+// 	[Sync]
+// 	public bool BlockMouseAim
+// 	{
+// 		get => BlockMovements || _blockMouseAim;
+// 		set => _blockMouseAim = value;
+// 	}
 
-	// 	var wishSpeed = Ducking ? DuckSpeed : isWalking ? WalkSpeed : SprintSpeed;
-	// 	var wishVelocity = Input.AnalogMove.Normal * wishSpeed * EyeAngles.WithPitch( 0f );
+// 	// public bool IsRunning { get; set; }
+// 	// protected override void OnEnabled()
+// 	// {
+// 	// 	base.OnEnabled();
 
-	// 	// // // Apply encumbarance.
-	// 	// if ( IsEncumbered )
-	// 	// {
-	// 	// 	var overweight = 10f;
-	// 	// 	var multiplier = MathX.Clamp( 1 - overweight / 15f, 0.2f, 1f );
-	// 	// 	wishVelocity *= multiplier;
-	// 	// }
+// 	// 	if ( IsProxy )
+// 	// 		return;
 
-	// 	MoveHelper.WishVelocity = BlockInputs ? Vector3.Zero : wishVelocity;
+// 	// 	var cam = Scene.GetAllComponents<CameraComponent>().FirstOrDefault();
+// 	// 	if ( cam is not null )
+// 	// 	{
+// 	// 		var ee = cam.Transform.Rotation.Angles();
+// 	// 		ee.roll = 0;
+// 	// 		EyeAngles = ee;
+// 	// 	}
+// 	// }
 
-	// 	if ( !BlockInputs && Input.Pressed( InputAction.Jump ) && MoveHelper.IsOnGround )
-	// 	{
-	// 		PlayerBody?.Set( "jump", true );
-	// 		// JumpBroadcast();
-	// 		OnJump( fJumps, "Hello", new object[] { Time.Now.ToString(), 43.0f }, Vector3.Random );
-	// 	}
+// 	protected void ControllerOnUpdate()
+// 	{
+// 		var movement = Input.AnalogMove.Normal;
+// 		var angles = EyeAngles;
+// 		var newRotationAngles = new Angles( 0, EyeAngles.yaw, 0 );
 
-	// 	// Ducking
-	// 	var from = Transform.Position + Vector3.Up * 5f;
-	// 	var to = from + Vector3.Up * (HEIGHT - 5f);
+// 		if ( Input.Down( "run" ) )
+// 			Body.Transform.Rotation = newRotationAngles;
+// 		else
+// 			Body.Transform.Rotation = Rotation.Lerp(  newRotationAngles , angles.ToRotation(), Time.Delta * 2f);
 
-	// 	// If we block inputs let's just keep whatever ducking state you were in
-	// 	if ( !BlockInputs )
-	// 	{
-	// 		Ducking = (Ducking && Scene.Trace.Ray( in from, in to ).Size( Collider.Scale.WithZ( 0f ) )
-	// 					  .IgnoreGameObjectHierarchy( GameObject ).WithoutTags( "trigger" ).Run().Hit)
-	// 				  || Input.Down( InputAction.Duck ); // Beautiful.
-	// 	}
+// 		var isWalking = Input.Down( InputAction.Walk );
 
-	// 	MoveHelper.Move();
+// 		var wishSpeed = Ducking ? DuckSpeed : isWalking ? WalkSpeed : SprintSpeed;
 
-	// 	var diff = MathF.Abs( Velocity.z - previousFallSpeed );
-	// 	if ( diff > 600 && previousFallSpeed < Velocity.z && !ForceHidePenoid ) // ForceHidePenoid, hack
-	// 	{
-	// 		var time = MathX.Clamp( 1, 4, diff / 250f );
-	// 		// SetRagdoll( true, duration: time );
-	// 		// GameObject.PlaySound( "impact" );
-	// 	}
+// 		var wishVelocity = Input.AnalogMove.Normal * wishSpeed * EyeAngles.WithPitch( 0f );
+// 		MoveDirection = wishVelocity;
+// 	}
 
-	// 	// Update Collider
-	// 	var height = Ducking ? DUCK_HEIGHT : HEIGHT;
-	// 	var bbox = MoveHelper.CollisionBBox;
+// 	[Broadcast]
+// 	public void OnJump( float floatValue, string dataString, object[] objects, Vector3 position )
+// 	{
+// 		AnimationHelper?.TriggerJump();
+// 	}
 
-	// 	MoveHelper.CollisionBBox = new BBox( bbox.Mins, bbox.Maxs.WithZ( height ) );
-	// 	Collider.Scale = Collider.Scale.WithZ( height );
-	// 	Collider.Center = Vector3.Up * height / 2f;
-	// }
+// 	float fJumps;
+// 	protected void UpdateController()
+// 	{
+// 		if ( IsProxy )
+// 			return;
+
+// 		BuildWishVelocity();
+
+// 		var cc = GameObject.Components.Get<CharacterController>();
+
+// 		if ( cc.IsOnGround && Input.Down( "Jump" ) )
+// 		{
+// 			float flGroundFactor = 1.0f;
+// 			float flMul = 268.3281572999747f * 1.2f;
+
+// 			cc.Punch( Vector3.Up * flMul * flGroundFactor );
+
+// 			OnJump( fJumps, "Hello", new object[] { Time.Now.ToString(), 43.0f }, Vector3.Random );
+
+// 			fJumps += 1.0f;
+// 		}
+
+// 		if ( cc.IsOnGround )
+// 		{
+// 			cc.Velocity = cc.Velocity.WithZ( 0 );
+// 			cc.Accelerate( WishVelocity );
+// 			cc.ApplyFriction( 4.0f );
+// 		}
+// 		else
+// 		{
+// 			cc.Velocity -= Gravity * Time.Delta * 0.5f;
+// 			cc.Accelerate( WishVelocity.ClampLength( 50 ) );
+// 			cc.ApplyFriction( 0.1f );
+// 		}
+
+// 		cc.Move();
+// 		// MoveHelper.Move();
+
+// 		if ( !cc.IsOnGround )
+// 		{
+// 			cc.Velocity -= Gravity * Time.Delta * 0.5f;
+// 		}
+// 		else
+// 		{
+// 			cc.Velocity = cc.Velocity.WithZ( 0 );
+// 		}
+// 	}
+
+// 	private Angles _currentRecoil;
+// 	private Angles _previousRecoil;
+// 	protected void UpdateAngles()
+// 	{
+// 		if ( DebugCamera )
+// 		{
+// 			DebugCameraAngles += Input.AnalogLook;
+// 		}
+// 		if ( BlockMouseAim ) return;
+
+// 		var before = EyeAngles;
+// 		var ang = EyeAngles;
+// 		ang += Input.AnalogLook;
+// 		ang += _currentRecoil - _previousRecoil; // Apply recoil to eye angles.
+// 		ang.pitch = ang.pitch.Clamp( -89, 89 );
+
+// 		EyeAngles = ang;
+
+// 		// Calculate recoil.
+// 		var diff = (before - ang).AsVector3().Abs().Length.Clamp( 1, 15 );
+// 		_previousRecoil = _currentRecoil;
+// 		_currentRecoil = _currentRecoil.LerpTo( Angles.Zero, diff * Time.Delta );
+// 	}
+
+	
+// 	// protected void UpdateMovement()
+// 	// {
+// 	// 	if ( MoveHelper == null ) return;
+
+// 	// 	var previousFallSpeed = Velocity.z;
+// 	// 	var isWalking = Input.Down( InputAction.Walk );
+
+// 	// 	var wishSpeed = Ducking ? DuckSpeed : isWalking ? WalkSpeed : SprintSpeed;
+// 	// 	var wishVelocity = Input.AnalogMove.Normal * wishSpeed * EyeAngles.WithPitch( 0f );
+
+// 	// 	// // // Apply encumbarance.
+// 	// 	// if ( IsEncumbered )
+// 	// 	// {
+// 	// 	// 	var overweight = 10f;
+// 	// 	// 	var multiplier = MathX.Clamp( 1 - overweight / 15f, 0.2f, 1f );
+// 	// 	// 	wishVelocity *= multiplier;
+// 	// 	// }
+
+// 	// 	MoveHelper.WishVelocity = BlockInputs ? Vector3.Zero : wishVelocity;
+
+// 	// 	if ( !BlockInputs && Input.Pressed( InputAction.Jump ) && MoveHelper.IsOnGround )
+// 	// 	{
+// 	// 		PlayerBody?.Set( "jump", true );
+// 	// 		// JumpBroadcast();
+// 	// 		OnJump( fJumps, "Hello", new object[] { Time.Now.ToString(), 43.0f }, Vector3.Random );
+// 	// 	}
+
+// 	// 	// Ducking
+// 	// 	var from = Transform.Position + Vector3.Up * 5f;
+// 	// 	var to = from + Vector3.Up * (HEIGHT - 5f);
+
+// 	// 	// If we block inputs let's just keep whatever ducking state you were in
+// 	// 	if ( !BlockInputs )
+// 	// 	{
+// 	// 		Ducking = (Ducking && Scene.Trace.Ray( in from, in to ).Size( Collider.Scale.WithZ( 0f ) )
+// 	// 					  .IgnoreGameObjectHierarchy( GameObject ).WithoutTags( "trigger" ).Run().Hit)
+// 	// 				  || Input.Down( InputAction.Duck ); // Beautiful.
+// 	// 	}
+
+// 	// 	MoveHelper.Move();
+
+// 	// 	var diff = MathF.Abs( Velocity.z - previousFallSpeed );
+// 	// 	if ( diff > 600 && previousFallSpeed < Velocity.z && !ForceHidePenoid ) // ForceHidePenoid, hack
+// 	// 	{
+// 	// 		var time = MathX.Clamp( 1, 4, diff / 250f );
+// 	// 		// SetRagdoll( true, duration: time );
+// 	// 		// GameObject.PlaySound( "impact" );
+// 	// 	}
+
+// 	// 	// Update Collider
+// 	// 	var height = Ducking ? DUCK_HEIGHT : HEIGHT;
+// 	// 	var bbox = MoveHelper.CollisionBBox;
+
+// 	// 	MoveHelper.CollisionBBox = new BBox( bbox.Mins, bbox.Maxs.WithZ( height ) );
+// 	// 	Collider.Scale = Collider.Scale.WithZ( height );
+// 	// 	Collider.Center = Vector3.Up * height / 2f;
+// 	// }
 
 
-	public void BuildWishVelocity()
-	{
-		var rot = EyeAngles.ToRotation();
+// 	public void BuildWishVelocity()
+// 	{
+// 		var rot = EyeAngles.ToRotation();
 
-		WishVelocity = rot * MoveDirection;
-		WishVelocity = WishVelocity.WithZ( 0 );
-		WishVelocity *= Rotation.From( 0f, 0f, 0f );
+// 		WishVelocity = rot * MoveDirection;
+// 		WishVelocity = WishVelocity.WithZ( 0 );
+// 		WishVelocity *= Rotation.From( 0f, 0f, 0f );
 
-		if ( !WishVelocity.IsNearZeroLength ) WishVelocity = WishVelocity.Normal;
+// 		if ( !WishVelocity.IsNearZeroLength ) WishVelocity = WishVelocity.Normal;
 
-		if ( Input.Down( "Run" ) ) WishVelocity *= 320.0f;
-		else WishVelocity *= 110.0f;
-	}
-}
+// 		if ( Input.Down( "Run" ) ) WishVelocity *= 320.0f;
+// 		else WishVelocity *= 110.0f;
+// 	}
+// }
